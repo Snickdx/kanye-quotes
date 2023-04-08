ea9015a717ade4a2e08f248a0deb1685bf295e8b
Nicholas Mendez <snickdx@gmail.com>
2023-04-08 15:01:38 +0000

    first commit

---
+++ public/main.js
@@ -0,0 +1,19 @@
+async function getData(){
+  let response = await fetch('https://api.kanye.rest/');
+  let data = await response.json();
+  renderQuote(data);
+}
+
+
+function renderQuote(data){
+  let result = document.querySelector('#result');
+  result.innerHTML = 
+          `<div class="alert alert-primary" role="alert">
+               ${data.quote}
+          </div>`;
+}
+
+getData();
+
+let myBtn = document.querySelector('#myBtn');
+myBtn.addEventListener('click', getData);
